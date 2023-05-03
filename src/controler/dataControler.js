085f8ac259f26e8d6656415938e2a5ba9a606c0b
teonateona12 <teona.piranishvili1@gmail.com>
2023-05-03 17:41:44 +0400

    feat: add delete functionality

--- src/controler/dataControler.js
+++ src/controler/dataControler.js
@@ -47,3 +47,14 @@ export function updateData(req, res) {
 
   res.send(data[index]);
 }
+export function deleteData(req, res) {
+  const data = JSON.parse(fs.readFileSync("data.json"));
+  const id = req.params.id;
+  const index = data.findIndex((item) => item.id == id);
+  if (index === -1) {
+    return res.status(404).send("Data not found");
+  }
+  const deletedItem = data.splice(index, 1)[0];
+  fs.writeFileSync("data.json", JSON.stringify(data));
+  res.send(deletedItem);
+}
--- src/routes/data.js
+++ src/routes/data.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { addData, getData, updateData } from "../controler/dataControler.js";
+import {
+  addData,
+  deleteData,
+  getData,
+  updateData,
+} from "../controler/dataControler.js";
 const data = Router();
 data.get("/getdata", getData);
 data.post("/addata", addData);
 data.put("/addata/:id", updateData);
+data.delete("/addata/:id", deleteData);
 
 export default data;

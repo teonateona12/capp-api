4659c2d3454812eaf42400dce958d459cf311a06
teonateona12 <teona.piranishvili1@gmail.com>
2023-05-03 17:31:22 +0400

    feat: add put method

--- src/controler/dataControler.js
+++ src/controler/dataControler.js
@@ -30,3 +30,20 @@ export function addData(req, res) {
     }
   });
 }
+
+export function updateData(req, res) {
+  const data = JSON.parse(fs.readFileSync("data.json"));
+  const id = req.params.id;
+
+  const updateData = req.body;
+  const index = data.findIndex((item) => item.id == id);
+  if (index === -1) {
+    return res.status(404).send("Data not found");
+  }
+
+  data[index] = { ...data[index], ...updateData };
+
+  fs.writeFileSync("data.json", JSON.stringify(data));
+
+  res.send(data[index]);
+}
--- src/routes/data.js
+++ src/routes/data.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { addData, getData } from "../controler/dataControler.js";
+import { addData, getData, updateData } from "../controler/dataControler.js";
 const data = Router();
 data.get("/getdata", getData);
 data.post("/addata", addData);
+data.put("/addata/:id", updateData);
 
 export default data;
